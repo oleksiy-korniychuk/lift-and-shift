593c13f2180b3fd79ec0739a00a9785a900a75fd
Oleksiy <okorniychuk@proton.me>
2024-08-27 00:30:48 -0500

    Added s3 static site origin to cors config

--- src/main/java/com/oleksiyk/lift_and_shift/WebConfig.java
+++ src/main/java/com/oleksiyk/lift_and_shift/WebConfig.java
@@ -14,7 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://lift-and-shift-ui.s3-website.us-east-2.amazonaws.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
