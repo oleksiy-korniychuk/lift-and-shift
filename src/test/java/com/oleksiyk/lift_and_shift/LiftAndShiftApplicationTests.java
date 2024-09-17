62ccf7642ef2beccbfa0e87952f34f8e06026ca6
Oleksiy <okorniychuk@proton.me>
2024-09-17 13:05:13 -0500

    Removed @SpringBootTest annotation from ApplicationTests class since I don't want it loading the db context along with SpringBoot

--- src/test/java/com/oleksiyk/lift_and_shift/LiftAndShiftApplicationTests.java
+++ src/test/java/com/oleksiyk/lift_and_shift/LiftAndShiftApplicationTests.java
@@ -1,11 +1,8 @@
 package com.oleksiyk.lift_and_shift;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LiftAndShiftApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
