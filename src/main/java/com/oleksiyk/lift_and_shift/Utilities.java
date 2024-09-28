4a318b1d2843bcbba43f2a10a78df92379ebb036
Oleksiy <okorniychuk@proton.me>
2024-09-28 14:49:34 -0500

    Got coors working with the front end and cookies are now authenticating correctly, locally.

--- src/main/java/com/oleksiyk/lift_and_shift/Utilities.java
+++ src/main/java/com/oleksiyk/lift_and_shift/Utilities.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
 
 public class Utilities {
     public static Cookie getTokenCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null || cookies.length == 0) {
+            return null;
+        }
         return Arrays.stream(request.getCookies())
                 .filter(cookie -> "las_token".equals(cookie.getName()))
                 .findFirst()
--- src/main/java/com/oleksiyk/lift_and_shift/configuration/SecurityConfiguration.java
+++ src/main/java/com/oleksiyk/lift_and_shift/configuration/SecurityConfiguration.java
@@ -13,6 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
 public class SecurityConfiguration {
@@ -24,17 +30,31 @@ public class SecurityConfiguration {
         http
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/hello").permitAll()
                         .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults())
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                ).httpBasic(Customizer.withDefaults());
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://lift-and-shift-ui.s3-website.us-east-2.amazonaws.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Requester-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
